fd71a7993f40b7cdf6a2aa0e7e6715265f80d0c6
John O'Keefe <jokeefe@fastmail.com>
2023-10-07 22:50:12 -0400

    added vim-visual-multi cursors

---
+++ plugins/community.lua
@@ -0,0 +1,20 @@
+return {
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  -- example of importing a plugin, comment out to use it or add your own
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.zig" },
+  { import = "astrocommunity.pack.html-css" },
+  -- { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.note-taking.obsidian-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
+}
---
+++ plugins/vim-visual-multi.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "mg979/vim-visual-multi",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function() end,
+  },
+}
