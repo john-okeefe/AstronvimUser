039a2455006f6430b4d432c13903df7c5702bab0
John O'Keefe <jokeefe@fastmail.com>
2023-10-30 12:49:01 -0400

    added neorg

--- plugins/community.lua
+++ plugins/community.lua
@@ -16,4 +16,5 @@ return {
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.motion.nvim-surround" },
+  -- { import = "astrocommunity.note-taking.neorg" },
 }
