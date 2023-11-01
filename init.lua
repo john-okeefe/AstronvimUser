c1cfbdd7fd52b727787a8b3c37d543b1f353741b
John O'Keefe <jokeefe@fastmail.com>
2023-10-31 21:25:46 -0400

    playing with image.nvim

--- init.lua
+++ init.lua
@@ -1,4 +1,14 @@
 return {
+  -- package = {
+  --   path = {
+  --     -- Example for configuring Neovim to load user-installed installed Lua rocks:
+  --     package.path
+  --       .. ";"
+  --       .. vim.fn.expand "$HOME"
+  --       .. "/.luarocks/share/lua/5.1/magick/init.lua;",
+  --   },
+  -- },
+
   colorscheme = "tokyonight",
 
   toggleterm = {
