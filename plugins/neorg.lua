e73eab60afa70304eeeaba6df2ced09ee0b85750
John O'Keefe <jokeefe@fastmail.com>
2023-12-03 11:09:54 -0500

    commented out neorg functionality

--- plugins/neorg.lua
+++ plugins/neorg.lua
@@ -1,35 +1,35 @@
 return {
-  "nvim-neorg/neorg",
-  run = ":Neorg sync-parsers",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  event = "VeryLazy",
-  opts = {
-    load = {
-      ["core.defaults"] = {}, -- Loads default behaviour
-      ["core.concealer"] = {}, -- Adds pretty icons to your documents
-      ["core.keybinds"] = {}, -- Adds default keybindings
-      ["core.completion"] = {
-        config = {
-          engine = "nvim-cmp",
-        },
-      }, -- Enables support for completion plugins
-      ["core.journal"] = {}, -- Enables support for the journal module
-      ["core.dirman"] = { -- Manages Neorg workspaces
-        config = {
-          workspaces = {
-            personal = "~/Nextcloud/Documents/neorgNotes/Personal",
-            bible = "~/Nextcloud/Documents/neorgNotes/Bible & Christianity",
-            gaming = "~/Nextcloud/Documents/neorgNotes/Gaming",
-            tech = "~/Nextcloud/Documents/neorgNotes/Technology",
-            media = "~/Nextcloud/Documents/neorgNotes/Video & Audio Editing",
-          },
-          -- Automatically detect whenever we have entered a subdirectory of a workspace
-          autodetect = true,
-
-          -- Automatically change the directory to the root of the workspace every time
-          autochdir = true,
-        },
-      },
-    },
-  },
+  --   "nvim-neorg/neorg",
+  --   run = ":Neorg sync-parsers",
+  --   dependencies = { "nvim-lua/plenary.nvim" },
+  --   event = "VeryLazy",
+  --   opts = {
+  --     load = {
+  --       ["core.defaults"] = {}, -- Loads default behaviour
+  --       ["core.concealer"] = {}, -- Adds pretty icons to your documents
+  --       ["core.keybinds"] = {}, -- Adds default keybindings
+  --       ["core.completion"] = {
+  --         config = {
+  --           engine = "nvim-cmp",
+  --         },
+  --       }, -- Enables support for completion plugins
+  --       ["core.journal"] = {}, -- Enables support for the journal module
+  --       ["core.dirman"] = { -- Manages Neorg workspaces
+  --         config = {
+  --           workspaces = {
+  --             personal = "~/Nextcloud/Documents/neorgNotes/Personal",
+  --             bible = "~/Nextcloud/Documents/neorgNotes/Bible & Christianity",
+  --             gaming = "~/Nextcloud/Documents/neorgNotes/Gaming",
+  --             tech = "~/Nextcloud/Documents/neorgNotes/Technology",
+  --             media = "~/Nextcloud/Documents/neorgNotes/Video & Audio Editing",
+  --           },
+  --           -- Automatically detect whenever we have entered a subdirectory of a workspace
+  --           autodetect = true,
+  --
+  --           -- Automatically change the directory to the root of the workspace every time
+  --           autochdir = true,
+  --         },
+  --       },
+  --     },
+  --   },
 }
